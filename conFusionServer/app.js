48b9a1c4b1c723b7a3472fb377339701c8a52747
Umair Arshad <umair6622@gmail.com>
2020-04-23 15:49:41 +0500

    Express Generator REST API

--- conFusionServer/app.js
+++ conFusionServer/app.js
@@ -7,6 +7,9 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+var dishRouter = require('./routes/dishRouter');
+var promoRouter = require('./routes/promoRouter');
+var leaderRouter = require('./routes/leaders');
 var app = express();
 
 // view engine setup
@@ -21,6 +24,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/dishes',dishRouter);
+app.use('/promotions',promoRouter);
+app.use('/leaders',leaderRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ conFusionServer/routes/dishRouter.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const dishRouter = express.Router();
+
+dishRouter.use(bodyParser.json());
+dishRouter.route('/')
+    .all((req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+})
+    .get((req, res, next) => {
+    res.end('Will send all the dishes to you!');
+})
+    .post((req, res, next) => {
+    res.end('Will add the dishes : '+ req.body.name +
+        ' with details: '+ req.body.description);
+})
+    .put((req, res, next) => {
+    res.statusCode = 403;
+    res.end('PUT operation not supported on /dishes')
+})
+    .delete((req, res, next) => {
+    res.end('Deleting all the dishes');
+});
+    dishRouter.route('/:dishId').all(function(req, res, next) {
+    res.writeHead(200, { 'Content-Type': 'type/plain' });
+    next();
+})
+    .get(function(req, res, next) {
+        res.end('Will send details of the dish: ' + req.params.dishId + ' to you!');
+    })
+    .put(function(req, res, next) {
+        res.write('Updating the dish: '+ req.params.dishId + '.\n');
+        res.end('Will update the dish: ' + req.body.name + ' with details: ' + req.body.description);
+    })
+    .post((req, res, next) => {
+       res.end('Will not add the leaders');
+    })
+    .delete(function(req, res, next) {
+        res.end('Deleting dish: ' + req.params.dishId);
+    });
+
+module.exports=dishRouter;
